fa8dbe017f2ebf19ad7bf935181cf83c79abbe81
Amey Anita Arun Thorat <88768050+thoratamey@users.noreply.github.com>
2025-07-08 23:40:15 +0530

    Create reverseword.py

---
+++ reverseword.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s=list(s.split())
+        l=0
+        r=len(s)-1
+        while l<=r:
+            s[l],s[r]=s[r],s[l]
+            l+=1
+            r-=1
+        return " ".join(s)
